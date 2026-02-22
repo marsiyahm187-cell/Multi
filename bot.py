dfb24ab126ec7991c80d3142c0478ff6a7100722
marsiyahm187-cell <marsiyahm187@gmail.com>
2026-02-22 19:50:19 +0700

    Fix formatting issues in bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,10 @@
 import time import json import os import threading import requests
 import feedparser
 
-BOT_TOKEN = os.getenv(“BOT_TOKEN”) OWNER_CHAT_ID = str(os.getenv(“OWNER_CHAT_ID”))
+BOT_TOKEN = os.getenv(“BOT_TOKEN”) 
+OWNER_CHAT_ID =str(os.getenv(“OWNER_CHAT_ID”))
 
-API = f”https://api.telegram.org/bot{BOT_TOKEN}” DATA_FILE = “users.json”
+API = f”https://api.telegram.org/bot{BOT_TOKEN}” DATA_FILE =“users.json”
 
 ================= DATA =================
 
